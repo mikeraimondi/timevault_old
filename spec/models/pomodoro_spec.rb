739871ba4152b74012b6508dafb35737495ab334
Michael Raimondi <zeno@pobox.com>
2013-05-21 09:09:10 -0400

    Re-implement edit account page with simple_form

--- spec/models/pomodoro_spec.rb
+++ spec/models/pomodoro_spec.rb
@@ -4,5 +4,7 @@ describe Pomodoro do
   it { should belong_to( :user ) }
   it { should validate_presence_of( :user ) }
 
+  it { should validate_presence_of( :duration ) }
+
   it { should have_many( :intervals ) }
 end
