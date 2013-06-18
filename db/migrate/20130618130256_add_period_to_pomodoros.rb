4f8f6c4e8f9ebe1bedd6bc0f800a23c0060a877e
Michael Raimondi <zeno@pobox.com>
2013-06-18 11:32:51 -0400

    Fix bug with migration adding periods to Pomodoros

--- db/migrate/20130618130256_add_period_to_pomodoros.rb
+++ db/migrate/20130618130256_add_period_to_pomodoros.rb
@@ -1,7 +1,8 @@
 class AddPeriodToPomodoros < ActiveRecord::Migration
   def up
-    add_column :pomodoros, :period, :string, null: false
-    Pomodoro.destroy_all
+    add_column :pomodoros, :period, :string
+    Pomodoro.update_all(period: "productive")
+    change_column :pomodoros, :period, :string, null: false
   end
 
   def down
