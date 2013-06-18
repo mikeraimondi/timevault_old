cce643889d31d57fc4065ca0e644c0cd23068e0b
Michael Raimondi <zeno@pobox.com>
2013-06-18 11:49:47 -0400

    Implement differing progress bar colors.
    Progress bar colors are now per period type. Minor fix to db:seed
    to set confirmation date on users.

--- app/presenters/pomodoro_indicator.rb
+++ app/presenters/pomodoro_indicator.rb
@@ -4,6 +4,26 @@ class PomodoroIndicator
     @pomodoro = pomodoro
   end
 
+  def bar_class
+    if @pomodoro.period == "productive"
+      "progress-info"
+    elsif @pomodoro.period == "break"
+      "progress-success"
+    elsif @pomodoro.period == "long_break"
+      "progress-warning"
+    end
+  end
+
+  def well_class
+    if @pomodoro.period == "productive"
+      "productive-entry"
+    elsif @pomodoro.period == "break"
+      "break-entry"
+    elsif @pomodoro.period == "long_break"
+      "long-break-entry"
+    end
+  end
+
   def animated
     @pomodoro.paused? ? "" : "active"
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
 require 'factory_girl_rails'
 
 user = FactoryGirl.create(:user)
+user.confirmed_at = Time.now
+user.save
